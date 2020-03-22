507385d48f400fc60abd3e610bb5ce949638bb65
Hannah Cook <hkcook30@gmail.com>
2020-03-21 23:51:05 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function onReady() {
+    const addToDoForm = document.getElementById('addToDoForm');
+    const newToDoText = document.getElementById('newToDoText');
+    const toDoList = document.getElementById('toDoList');
+
+      addToDoForm.addEventListener();
+}
+
+window.onload = function() {
+  alert("The window has loaded!");
+    onReady();
+};
