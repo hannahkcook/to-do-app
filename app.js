0259d3067f76e0f6b0f4028ba5dec2de23c3d53f
Hannah Cook <hkcook30@gmail.com>
2020-04-17 16:24:56 -0500

    dom_checkpoint_2

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 function onReady() {
+  let id = 0;
   let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
 
   function createNewToDo() {
     const newToDoText = document.getElementById('newToDoText');
     if (!newToDoText.value) { return; }
-    
+
     toDos.push({
     title: newToDoText.value,
     complete: false
@@ -13,29 +14,30 @@ function onReady() {
    newToDoText.value = '';
 
    renderTheUI();
- }
-  function renderTheUI() {
-      const toDoList = document.getElementById('toDoList');
-
-         toDoList.textContent = '';
-
-         toDos.forEach(function(toDo) {
-           const newLi = document.createElement('li');
-           const checkbox = document.createElement('input');
-           checkbox.type = "checkbox";
-
-           newLi.textContent = toDo.title;
-
-           toDoList.appendChild(newLi);
-           newLi.appendChild(checkbox);
-  });
 
  }
+
+function renderTheUI() {
+  const toDoList = document.getElementById('toDoList');
+
+  toDoList.textContent = '';
+
+  toDos.forEach(function(toDo) {
+     const newLi = document.createElement('li');
+     const checkbox = document.createElement('input');
+     checkbox.type = "checkbox";
+
+     newLi.textContent = toDo.title;
+
+     toDoList.appendChild(newLi);
+     newLi.appendChild(checkbox);
+   });
+}
   addToDoForm.addEventListener('submit', event => {
    event.preventDefault();
    createNewToDo();
  });
- renderTheUI();
+
 }
 window.onload = function() {
   alert("The window has loaded!");
